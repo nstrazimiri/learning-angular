4e4c01690c19deb151c8d084a79e759d6a61884c
Ndriçim Strazimiri <w0730680@myscc.ca>
2020-11-15 14:42:26 -0500

    Added functionality to deliver content using obsarvable service pattern

--- angularApp/src/app/services/content.service.ts
+++ angularApp/src/app/services/content.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Content} from '../helper-files/content-interface';
+import {CONTENTLIST} from '../helper-files/contentDb';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,11 @@ import { Injectable } from '@angular/core';
 export class ContentService {
 
   constructor() { }
+
+  getContents(): Content[] { // synchronous
+    return CONTENTLIST;
+  }
+  getContentsObs(): Observable<Content[]>{ // asynchronous
+    return of(CONTENTLIST);
+  }
 }
